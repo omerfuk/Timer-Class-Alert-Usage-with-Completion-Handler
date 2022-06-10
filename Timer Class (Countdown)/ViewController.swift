60f4937392ddc28dda6c4b03b97e1f700d3d2c6e
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-10 09:33:24 +0300

    .

--- Timer Class (Countdown)/ViewController.swift
+++ Timer Class (Countdown)/ViewController.swift
@@ -8,10 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timerClass = Timer()
+    var counter = 0
+    
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counter = 10
+        textLabel.text = "Timer : \(counter)"
+        timerClass = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        
+
+    }
+    
+    @objc func timerCounter() {
+        
+        counter -= 1
+        textLabel.text = "Timer : \(counter)"
+        if counter == 0 {
+         timerClass.invalidate()
+            textLabel.text = "Time is OVER"
+            
+            
+            let alert = callAlert(title1: "Warning", message1: "Time IS OVER")
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func callAlert(title1: String, message1: String) -> UIAlertController {
+        let alert = UIAlertController(title: title1, message: message1, preferredStyle: .alert)
+        
+        let stayInFirstViewAction = UIAlertAction(title: "Stay", style: .default, handler: nil)
+        let goToSecondViewAction = UIAlertAction(title: "GO", style: .destructive) { alert in
+            
+            self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+        }
+        
+        
+        alert.addAction(stayInFirstViewAction)
+        alert.addAction(goToSecondViewAction)
+        
+        
+        return alert
+        
     }
 
 
